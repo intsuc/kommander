29c33d4f87f539c9aa775d1d2256e062800fa272
intsuc <intsuc@gmail.com>
2025-01-10 17:35:49 +0900

    Port

--- src/commonMain/kotlin/dev/intsuc/kommander/context/CommandContext.kt
+++ src/commonMain/kotlin/dev/intsuc/kommander/context/CommandContext.kt
@@ -1,3 +1,73 @@
 package dev.intsuc.kommander.context
 
-class CommandContext<S>
+import dev.intsuc.kommander.Command
+import dev.intsuc.kommander.RedirectModifier
+import dev.intsuc.kommander.tree.CommandNode
+
+class CommandContext<S>(
+    val source: S,
+    val input: String,
+    val arguments: Map<String, ParsedArgument<S, *>>,
+    val command: Command<S>?,
+    val rootNode: CommandNode<S>,
+    val nodes: List<ParsedCommandNode<S>>,
+    val range: StringRange,
+    val child: CommandContext<S>?,
+    val redirectModifier: RedirectModifier<S>,
+    private val forks: Boolean,
+) {
+    fun copyFor(source: S): CommandContext<S> {
+        if (this.source === source) {
+            return this
+        }
+        return CommandContext(source, input, arguments, command, rootNode, nodes, range, child, redirectModifier, forks)
+    }
+
+    val lastChild: CommandContext<S>
+        get() {
+            var result = this
+            while (result.child != null) {
+                result = result.child!!
+            }
+            return result
+        }
+
+    inline fun <reified V> getArgument(name: String): V {
+        val argument = arguments[name] ?: throw IllegalArgumentException("No such argument '$name' exists on this command")
+
+        val result = argument.result
+        return if (result is V) {
+            result
+        } else {
+            throw IllegalArgumentException("Argument '$name' is defined as ${result!!::class.simpleName}, not ${V::class.simpleName}")
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CommandContext<*>) return false
+
+        if (arguments != other.arguments) return false
+        if (rootNode != other.rootNode) return false
+        if (nodes.size != other.nodes.size || nodes != other.nodes) return false
+        if (if (command != null) command != other.command else other.command != null) return false
+        if (source != other.source) return false
+        if (if (child != null) child != other.child else other.child != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = source.hashCode()
+        result = 31 * result + arguments.hashCode()
+        result = 31 * result + command.hashCode()
+        result = 31 * result + rootNode.hashCode()
+        result = 31 * result + nodes.hashCode()
+        result = 31 * result + child.hashCode()
+        return result
+    }
+
+    fun hasNodes(): Boolean = nodes.isNotEmpty()
+
+    fun isForked(): Boolean = forks
+}
--- src/commonMain/kotlin/dev/intsuc/kommander/context/CommandContextBuilder.kt
+++ src/commonMain/kotlin/dev/intsuc/kommander/context/CommandContextBuilder.kt
@@ -1,3 +1,114 @@
 package dev.intsuc.kommander.context
 
-class CommandContextBuilder<S>
+import dev.intsuc.kommander.Command
+import dev.intsuc.kommander.CommandDispatcher
+import dev.intsuc.kommander.RedirectModifier
+import dev.intsuc.kommander.tree.CommandNode
+
+class CommandContextBuilder<S> {
+    private val _arguments: MutableMap<String, ParsedArgument<S, *>> = linkedMapOf()
+    private val rootNode: CommandNode<S>
+    private val _nodes: MutableList<ParsedCommandNode<S>> = arrayListOf()
+    val dispatcher: CommandDispatcher<S>
+    var source: S? = null
+        private set
+    var command: Command<S>? = null
+        private set
+    private var child: CommandContextBuilder<S>? = null
+    var range: StringRange? = null
+        private set
+    private var modifier: RedirectModifier<S>? = null
+    private var forks: Boolean = false
+
+    constructor(dispatcher: CommandDispatcher<S>, source: S, rootNode: CommandNode<S>, start: Int) {
+        this.rootNode = rootNode
+        this.dispatcher = dispatcher
+        this.source = source
+        this.range = StringRange.at(start)
+    }
+
+    fun withSource(source: S): CommandContextBuilder<S> {
+        this.source = source
+        return this
+    }
+
+    fun withArgument(name: String, argument: ParsedArgument<S, *>): CommandContextBuilder<S> {
+        _arguments[name] = argument
+        return this
+    }
+
+    val arguments: Map<String, ParsedArgument<S, *>> get() = _arguments
+
+    fun withCommand(command: Command<S>): CommandContextBuilder<S> {
+        this.command = command
+        return this
+    }
+
+    fun withNode(node: CommandNode<S>, range: StringRange): CommandContextBuilder<S> {
+        _nodes.add(ParsedCommandNode(node, range))
+        this.range = StringRange.encompassing(this.range!!, range)
+        this.modifier = node.redirectModifier
+        this.forks = node.isFork()
+        return this
+    }
+
+    fun copy(): CommandContextBuilder<S> {
+        val copy = CommandContextBuilder(dispatcher, source!!, rootNode, range!!.start)
+        copy.command = command
+        copy._arguments += _arguments
+        copy._nodes += _nodes
+        copy.child = child
+        copy.range = range
+        copy.forks = forks
+        return copy
+    }
+
+    fun withChild(child: CommandContextBuilder<S>): CommandContextBuilder<S> {
+        this.child = child
+        return this
+    }
+
+    val lastChild: CommandContextBuilder<S>
+        get() {
+            var result = this
+            while (result.child != null) {
+                result = result.child!!
+            }
+            return result
+        }
+
+    val nodes: List<ParsedCommandNode<S>> get() = _nodes
+
+    fun build(input: String): CommandContext<S> {
+        TODO()
+    }
+
+    fun findSuggestionContext(cursor: Int): SuggestionContext<S> {
+        if (range!!.start <= cursor) {
+            if (range!!.end < cursor) {
+                if (child != null) {
+                    return child!!.findSuggestionContext(cursor)
+                } else if (_nodes.isNotEmpty()) {
+                    val last = _nodes.last()
+                    return SuggestionContext(last.node, last.range.end + 1)
+                } else {
+                    return SuggestionContext(rootNode, range!!.start)
+                }
+            } else {
+                var prev = rootNode
+                for (node in _nodes) {
+                    val nodeRange = node.range
+                    if (nodeRange.start <= cursor && cursor < nodeRange.end) {
+                        return SuggestionContext(prev, nodeRange.start)
+                    }
+                    prev = node.node
+                }
+                if (prev == null) {
+                    throw IllegalStateException("Can't find node before cursor")
+                }
+                return SuggestionContext(prev, range!!.start)
+            }
+        }
+        throw IllegalStateException("Can't find node before cursor")
+    }
+}
